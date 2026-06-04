b8ec29e6c7a091c784571872199e77a301e02630
joshua <joshuapruitt6484@gmail.com>
2026-06-03 18:18:29 -0700

    Removed the parser and replaced it with the tokenizer. The app has now been simplified for further development

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,98 +1,20 @@
 import { useState } from 'react';
-import {add, mult, sub, div} from './components/functions.ts';
-import { parseText } from './components/parser.ts';
+import tokenize from './components/eval.ts';
 import './App.css'
 
 function App() {
-  const [input, updateInp1] = useState<number>(0);
-  const [input2, updateInp2] = useState<number>(0);
-  const [resultant, updateResultant] = useState<number>(0) //The result of an operation
-  const [operand, changeOperand] = useState<string>('')
+  const [resultant, updateResultant] = useState<string>('') //The result of an operation
 
   const [func, changeFunc] = useState<string>('');
-  const [opperandArr, changeOperandArr] = useState<string[]>([]);
-  const [numberArr, changeNumberArr] = useState<number[]>([]);
-  const [variableArr, changeVariableArr] = useState<string[]>([]);
 
   const equate = () => {
-    let res = 0;
-
-    if (operand === '+') {
-      res = additionHandler();
-    } else if (operand === '*') {
-      res = multiplicationHandler();
-    } else if (operand === '-') {
-      res = subtractionHandler();
-    } else if (operand === '/') {
-      res = divideHandler();
-    }
-
-    updateResultant(res);
+    const tokens = tokenize([...func])
+    console.log(tokens)
+    updateResultant(JSON.stringify(tokens))
   };
-
-  const additionHandler = () => add(input, input2);
-  const multiplicationHandler = () => mult(input, input2);
-  const subtractionHandler = () => sub(input, input2)
-  const divideHandler = () => div(input, input2);
-
-  const parseNumber = (input: any) => {
-    return Number(input);
-  }
-
-  const ret = () => {
-    const {numArr, oppArr, varArr} = parseText(func);
-
-    changeOperandArr(oppArr);
-    changeNumberArr(numArr);
-    changeVariableArr(varArr);
-  }
 
   return (
     <div>
-      <h1>HI RUBY</h1>
-J
-      <div>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => updateInp1(parseNumber(e.target.value))}
-          placeholder="number 1"
-        />
-      </div>
-
-      <div>
-        <input
-          type="text"
-          value={input2}
-          onChange={(e) => updateInp2(parseNumber(e.target.value))}
-          placeholder="number 2"
-        />
-      </div>
-
-      <div>
-        <h3>{operand}</h3>
-      </div>
-
-      <button type="button" onClick={() => changeOperand('+')}>
-        <h2>+</h2>
-      </button>
-
-      <button type="button" onClick={() => changeOperand('*')}>
-        <h2>*</h2>
-      </button>
-
-      <button type="button" onClick={() => changeOperand('-')}>
-        <h2>-</h2>
-      </button>
-
-      <button type="button" onClick={() => changeOperand('/')}>
-        <h2>/</h2>
-      </button>
-
-      <button type="button" onClick={equate}>
-        <h2>=</h2>
-      </button>
-
       <div>
         <input 
           type='text'
@@ -100,17 +22,9 @@ J
         />
       </div>
 
-      <div>
-        <button type='button' onClick={() => ret()}>=</button>
-      </div>
-
-      <h2>{func}</h2>
-
-      <div>
-        <h2>{opperandArr.toString()}</h2>
-        <h2>{numberArr.toString()}</h2>
-        <h2>{variableArr.toString()}</h2>
-      </div>
+      <button type="button" onClick={equate}>
+        <h2>=</h2>
+      </button>
 
       <div>
         <span>{resultant}</span>
--- client/src/components/eval.ts
+++ client/src/components/eval.ts
@@ -1,33 +1,63 @@
-import {add, mult, sub, div} from './functions.ts';
-// Function evaluates the given inputs
+// SUDO
+// 1. Tokenize, from right to left take a given string of characters and group into tokens the computer can read
+// Turning values into tokens assigns a value a type the value. The type indicates the label, ect: NUM (a number, duh) , ID (A variable, ect: x or y), -
+// opperators (*, /, +, -), = (the equal sign), () grouping, EOF (marking the end of input)
 
-// add, multiply, subtract, divide numbers
-const additionHandler = (input: number, input2: number) => add(input, input2);
-const multiplicationHandler = (input: number, input2: number) => mult(input, input2);
-const subtractionHandler = (input: number, input2: number) => sub(input, input2)
-const divideHandler = (input: number, input2: number) => div(input, input2);
+// 2. Parse,
 
-// determine which opperation is being preformed then preform the opperation
-const determine_opperation = (input: number, input2: number, opperand: string) => {
+// const OPERATORS = {
+//     "*": 0,
+//     "/": 0,
+//     "+": 1,
+//     "-": 1
+// }
 
-    if (opperand === '+') {
-        return additionHandler(input, input2);
-    } else if (opperand === '*') {
-        return multiplicationHandler(input, input2);
-    } else if (opperand === '-') {
-        return subtractionHandler(input, input2);
-    } else if (opperand === '/') {
-        return divideHandler(input, input2);
-    } else {
-        return 0
+// Stage 1: Tokenizer 
+// From left to right take a input function and turn it into a list of tokens.
+// Each token has {type: "", val: }
+const tokenize = (src: string[]) => {
+    let tokens = [];
+    let i = 0;
+
+    while (i < src.length){
+
+        //Skip white space. 
+        if (/\s/.test(src[i])){ i++; continue;}
+
+        // test for numbers from 0 to 9
+        // missing functionality for decimals. 
+        if (/[0-9]/.test(src[i])){
+            let n = '';
+            // gets the full number instead of just the first number.
+            // add value to n if its a number. Anything else gets skipped
+            while (i < src.length && /[0-9]/.test(src[i])) n += src[i++];
+
+            tokens.push({ type: 'NUM', val: parseFloat(n)})
+            continue;
+        }
+
+        // test for variables
+        if (/[a-zA-Z ]/.test(src[i])){
+            let name = '';
+            // Get the full variable in cases where a variable might be something like xy
+            while (i < src.length && /[a-zA-Z ]/.test(src[i])) name += src[i++];
+            tokens.push({ type: 'ID', val: name});
+            continue;
+        }
+
+        // test for opperators
+        if ('*/+-()'.includes(src[i])){
+            tokens.push({ type: src[i], val: src[i]});
+            i++;
+            continue;
+        }
+
+        throw new Error(`Unknown Character ${src[i]}`);
     }
-};
 
-const eval = (oppArr: string[], numberArr: number[], varArr: string[]) => {
-    //iterate between all variables.
-    for (let i = 0; i < numberArr.length + varArr.length; i++){
-        
-    }
+    tokens.push({type: 'EOF'});
+    return tokens;
 }
 
-export default {eval};
+export default tokenize;
+
